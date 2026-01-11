59f467adbf96b74e7060bce4d0ce612cf1a264c6
Nilushiya <nilushiyak@gmail.com>
2026-01-11 18:49:56 +0530

    Fix imports for Netlify build

--- photo_studio/frontend/src/pages/user/user-dashboard/DashboardLayout.jsx
+++ photo_studio/frontend/src/pages/user/user-dashboard/DashboardLayout.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Headers from "./Header";
+import Headers from "./header";
 import Sidebar from "./Sidebar";
 import { Outlet } from "react-router-dom";
 import Navbar from "../../../components/Navbar";

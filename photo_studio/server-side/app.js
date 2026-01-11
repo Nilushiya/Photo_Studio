70b6bc27626f7c62ff3e781e00abf9f7ba547d08
Nilushiya <nilushiyak@gmail.com>
2026-01-11 21:40:20 +0530

    .

--- photo_studio/server-side/app.js
+++ photo_studio/server-side/app.js
@@ -21,6 +21,7 @@ app.use(cors({
   methods: ['GET','POST','PUT','DELETE','OPTIONS'],
   allowedHeaders: ['Content-Type','Authorization']
 }));
+app.options('*', cors());
 
 app.use('/api/client', clientRoutes)
 app.use('/api/services', serviceRoutes)
